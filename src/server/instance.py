6c34e615612b34129b1ed6ac40cd84c77b7fedc3
Lucas Kaminski <lucas.kssilveira@gmail.com>
2022-07-23 15:46:37 -0300

    fix: changed static path

--- src/server/instance.py
+++ src/server/instance.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 class Server:
   def __init__(self) -> None:
-    self.app = Flask(__name__, template_folder='../templates', static_folder='../templates/static')
+    self.app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
   def run(self) -> None:
     self.app.run(host='0.0.0.0', port=5000, debug=True)
